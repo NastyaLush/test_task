43aaef281ef3513d2944a9750208f0ec8f7ffedb
NastyaLush <yfcn.if1203@gmail.com>
2024-03-08 22:03:16 +0300

    done

--- script.py
+++ script.py
@@ -2,31 +2,68 @@ import csv
 import datetime
 import os
 import re
-from asyncio import Lock
+import logging
 from enum import Enum
+from threading import Thread, Lock
 
 import psycopg2
 import requests
 import json
 import time
-from datetime import date
 
 from dotenv import load_dotenv
 from psycopg2.extras import execute_batch
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from dateutil.parser import parse
 from bs4 import BeautifulSoup
 
-file_to_read_csv = "./articles.csv"
+file_to_read_csv = "./articles2.csv"
 
 
 class ArticleType(Enum):
     COIN_MARKET_CAP = "COIN_MARKET_CAP"
     WEB = "WEB"
+
+
 class Link(Enum):
     SELF = "_self"
     BLANK = "_blank"
+
+
+
+
+def start():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
+    load_dotenv()
+    with psycopg2.connect(
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+            dbname=os.getenv("POSTGRES_DB"),
+            host=os.getenv("POSTGRES_HOST"),
+            port=os.getenv("POSTGRES_PORT")
+    ) as conn:
+        with conn.cursor() as cursor:
+            with open(file_to_read_csv, 'w', newline='', encoding="utf-8") as csvfile:
+                fieldnames = ['heading', 'article_type', 'author', 'created_at', 'text', 'associated_tokens', 'link']
+                writer = csv.writer(csvfile)
+                writer.writerow(fieldnames)
+                options = Options()
+                options.add_argument('--no-sandbox')
+                options.add_argument('--disable-gpu')
+                options.add_argument('--disable-dev-shm-usage')
+                options.add_argument('--headless')
+                options.add_argument('--start-maximized')
+                driver = webdriver.Chrome(options=options)
+                logging.info(f"start time {datetime.datetime.now()}")
+
+                load_first_content(cursor, conn, writer, driver)
+                logging.info(f"finish time {datetime.datetime.now()}")
+            automatic_work(driver,
+                           f"https://coinmarketcap.com/community/articles/browse/?sort=-publishedOn&page=1&category=",
+                           cursor)
+            driver.close()
 
 
 def get_text_from_coin_market_cap_page(link):
@@ -41,8 +78,9 @@ def get_text_from_coin_market_cap_page(link):
 def automatic_work(driver, url, cursor):
     driver.get(url)
     while True:
-        print("do update")
+        logging.info("do update")
         driver.refresh()
+        driver.implicitly_wait(10)
         articles = get_lazy_data_from_page(driver, cursor)
         execute_batch(cursor, '''
                                     insert into articles(
@@ -54,76 +92,37 @@ def automatic_work(driver, url, cursor):
                                     associated_tokens,
                                     link) values (%s,%s,%s,%s,%s,%s,%s) on conflict(link) do nothing;
                                     ''', articles)
-        print(f"added articles:{articles} ")
+        logging.info(f"added articles:{articles} ")
         time.sleep(60)
-
-
-def start():
-    load_dotenv()
-    with psycopg2.connect(
-            user=os.getenv("POSTGRES_USER"),
-            password=os.getenv("POSTGRES_PASSWORD"),
-            dbname=os.getenv("POSTGRES_DB")
-    ) as conn:
-        with conn.cursor() as cursor:
-            with open(file_to_read_csv, 'w', newline='', encoding="utf-8") as csvfile:
-                fieldnames = ['heading', 'article_type', 'author', 'created_at', 'text', 'associated_tokens', 'link']
-                writer = csv.writer(csvfile)
-                writer.writerow(fieldnames)
-
-                driver = webdriver.Chrome()
-
-                load_first_content(cursor, conn, writer, driver)
-
-            automatic_work(driver,
-                           f"https://coinmarketcap.com/community/articles/browse/?sort=-publishedOn&page=1&category=",
-                           cursor)
-            driver.close()
-
 
 def load_first_content(cursor, conn, writer, driver, count_of_pages=10):
     lock = Lock()
+    cursor_lock = Lock()
+    writer_lock = Lock()
+    threads = list()
     for page in range(1, count_of_pages + 1):
-        print(f"start load page:{page}")
-        url_tmp = f"https://coinmarketcap.com/community/articles/browse/?sort=-publishedOn&page={page}&category="
-        lock.acquire()
-        driver.get(url_tmp)
-        driver.implicitly_wait(10)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        with open('out.html', 'w', encoding="utf-8") as f:
-            print(driver.page_source, "data.html", file=f)
-        print(f"start read page:{page}")
-        articles_data = get_articles_from_page(driver, lock)
-        print(f"got all articles from page {page}")
-        execute_batch(cursor, '''
-                            insert into articles(
-                            heading,
-                            article_type,
-                            author,
-                            created_at,
-                            text,
-                            associated_tokens,
-                            link) values (%s,%s,%s,%s,%s,%s,%s) on conflict(link) do nothing;
-                            ''', articles_data)
-        print(f"wrote all articles from page {page} to db")
-        for article in articles_data:
-            writer.writerow(article)
-        print(f"wrote all articles from page {page} to file")
-        conn.commit()
-        print(f"read page:{page}")
-def load(driver, conn, cursor, lock, page):
-    print(f"start load page:{page}")
+        thread = Thread(target=load, args=(driver, conn, cursor, page, lock, cursor_lock, writer_lock, writer))
+        threads.append(thread)
+        thread.start()
+    logging.info("wait all threads")
+    for thread in threads:
+        thread.join()
+    logging.info("threads completed work")
+    # load(driver, conn, cursor, page, lock, writer)
+
+
+def load(driver, conn, cursor, page, lock, cursor_lock, writer_lock, writer):
+    logging.info(f"start work with page:{page}")
     url_tmp = f"https://coinmarketcap.com/community/articles/browse/?sort=-publishedOn&page={page}&category="
     lock.acquire()
     driver.get(url_tmp)
     driver.implicitly_wait(10)
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    with open('out.html', 'w', encoding="utf-8") as f:
-        print(driver.page_source, "data.html", file=f)
-    print(f"start read page:{page}")
+    logging.info(f"start read page:{page}")
     articles_data = get_articles_from_page(driver, lock)
-    print(f"got all articles from page {page}")
-    execute_batch(cursor, '''
+    logging.info(f"got all articles from page {page}")
+    with cursor_lock:
+        execute_batch(cursor, '''
                                 insert into articles(
                                 heading,
                                 article_type,
@@ -133,12 +132,14 @@ def load(driver, conn, cursor, lock, page):
                                 associated_tokens,
                                 link) values (%s,%s,%s,%s,%s,%s,%s) on conflict(link) do nothing;
                                 ''', articles_data)
-    print(f"wrote all articles from page {page} to db")
-    for article in articles_data:
-        writer.writerow(article)
-    print(f"wrote all articles from page {page} to file")
+    logging.info(f"wrote all articles from page {page} to db")
+    with writer_lock:
+        for article in articles_data:
+            writer.writerow(article)
+    logging.info(f"wrote all articles from page {page} to file")
     conn.commit()
-    print(f"read page:{page}")
+    logging.info(f"read page:{page}")
+
 
 def get_articles_from_page(driver, lock):
     articles_list = list()
@@ -157,20 +158,24 @@ def get_articles_from_page(driver, lock):
         # link: str
         article, link = get_article(a)
         articles_list.append(article)
+        #
     return articles_list
 
 
 def get_lazy_data_from_page(driver, cursor):
-    articlesList = list()
-    driver.implicitly_wait(10)
-    contents = driver.find_element(By.CLASS_NAME, 'ibVHPJ').find_elements(By.CLASS_NAME, 'cmc-link')
-    for c in contents:
+    articles_list = list()
+    contents = driver.find_element(By.CLASS_NAME, 'ibVHPJ')
+    html = contents.get_attribute('innerHTML')
+    soup = BeautifulSoup(html, "html.parser")
+    articles = soup.find_all('a', class_="cmc-link")
+    for c in articles:
         article, link = get_article(c)
         if not check_if_article_exists_in_db(cursor, link):
-            articlesList.append(article)
+            articles_list.append(article)
         else:
-            return articlesList
-    return articlesList
+            return articles_list
+    return articles_list
+
 
 def check_if_article_exists_in_db(cursor, current_link):
     cursor.execute('''
@@ -185,12 +190,12 @@ def get_article(article):
 
     data = article.find('div', class_='ehRbwo')
     heading = data.find('p', class_='title').text
-    text = data.find('p', class_= 'description').text
+    text = data.find('p', class_='description').text
 
-    meta = data.find('div', class_= 'jRnXDB')
-    author = meta.find('div', class_= 'tooltip')["data-text"]
+    meta = data.find('div', class_='jRnXDB')
+    author = meta.find('div', class_='tooltip')["data-text"]
     created_at = parse_date(meta.find('div', class_='date-info').text)
-    for token in meta.find('div', class_= 'kHhYHG').find_all('span'):
+    for token in meta.find('div', class_='kHhYHG').find_all('span'):
         associated_tokens.append(token.text)
 
     if article["target"] == Link.SELF.value:
@@ -199,13 +204,15 @@ def get_article(article):
     elif article["target"] == Link.BLANK.value:
         article_type = ArticleType.WEB.value
     else:
-        raise Exception(f"Unknown article type {article.get_attribute("target")}")
+        raise Exception(f"Unknown article type {article['target']}")
     # print((heading, article_type, author, str(created_at), text, associated_tokens, link))
     return (heading, article_type, author, str(created_at), text, associated_tokens, link), link
+
+
 def parse_date(input):
-    hour_match = re.search(r"(\d{2})h", input)
-    min_match = re.search(r"(\d{2})m", input)
-    day_match = re.search(r"(\d{2})day", input)
+    hour_match = re.search(r"(\d{1,2})h", input)
+    min_match = re.search(r"(\d{1,2})m", input)
+    day_match = re.search(r"(\d{1,2})day", input)
 
     if hour_match:
         hours = int(hour_match.group(1))
@@ -218,5 +225,6 @@ def parse_date(input):
         return datetime.datetime.now() - datetime.timedelta(days=days)
     else:
         return parse(input)
+
 
 start()
